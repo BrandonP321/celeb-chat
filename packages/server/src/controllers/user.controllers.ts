3d9fa56878ba527a43f9365560f2f7fe426fcd0a
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-04-01 22:51:56 -0700

    Added GetUser API endpoint

---
+++ packages/server/src/controllers/user.controllers.ts
@@ -0,0 +1,11 @@
+import { GetUserRequest } from "@celeb-chat/shared/src/api/Requests/user.requests";
+import { TRouteController } from ".";
+import { TUserDocLocals } from "@/Middleware";
+
+/** Returns full user JSON without sensitive data */
+export const GetUserController: TRouteController<
+  GetUserRequest.Request,
+  TUserDocLocals
+> = async (req, res) => {
+  return res.json(await res.locals.user.toFullJSON()).end();
+};
--- packages/server/src/routes/user.routes.ts
+++ packages/server/src/routes/user.routes.ts
@@ -1,11 +1,16 @@
 import express from "express";
-// import { Routes } from "@goal-tracker/shared/src/api/routes";
-// import { GetFullUserController } from "~Controllers/user.controllers";
-// import { AuthJwt } from "~Middleware/AuthJWT";
-// import { GetUserMiddleware } from "~Middleware/GetUser.middleware";
+import { Routes } from "@celeb-chat/shared/src/api/routes";
+import { GetUserController } from "@/Controllers/user.controllers";
+import { AuthJwt } from "@/Middleware/AuthJWT";
+import { GetUserMiddleware } from "@/Middleware/GetUser.middleware";
 
 const router = express.Router();
 
-// router.get(Routes.User.GetFullUser({}), AuthJwt, GetUserMiddleware, GetFullUserController);
+router.get(
+  Routes.User.GetFullUser(),
+  AuthJwt,
+  GetUserMiddleware,
+  GetUserController
+);
 
 export default router;
