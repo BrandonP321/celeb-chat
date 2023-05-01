008c4fdcc7877678f49b49b78277cc685abc7776
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-04-30 17:45:32 -0700

    Added chat not found UI to chat edit page

--- packages/web/src/pages/EditChat/EditChat.tsx
+++ packages/web/src/pages/EditChat/EditChat.tsx
@@ -6,6 +6,7 @@ import { GetChatRequest } from "@celeb-chat/shared/src/api/Requests/chat.request
 import { useLocation } from "react-router-dom";
 import { WebChatUtils } from "utils/ChatUtils";
 import { APIFetcher } from "utils/APIFetcher";
+import { ChatNotFoundContent } from "pages/Chat/components/ChatNotFoundContent/ChatNotFoundContent";
 
 export namespace EditChat {
   export type Props = {};
@@ -13,17 +14,27 @@ export namespace EditChat {
 
 export function EditChat(props: EditChat.Props) {
   const [chat, setChat] = useState<GetChatRequest.Response | null>(null);
+  const [chatNotFound, setChatNotFound] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     const chatId = WebChatUtils.getChatIdFromEditUrl();
+    setChatNotFound(false);
 
-    chatId && APIFetcher.getChat({ chatId }).then(setChat);
+    chatId &&
+      APIFetcher.getChat({ chatId })
+        .then(setChat)
+        .catch((err: GetChatRequest.Error) => {
+          if (err.errCode === GetChatRequest.ErrorCode.ChatNotFound) {
+            setChatNotFound(true);
+          }
+        });
   }, [location]);
 
   return (
     <div className={styles.editChat}>
-      <LoadingContainer loading={!chat} />
+      <LoadingContainer loading={!chat && !chatNotFound} />
+      {chatNotFound && <ChatNotFoundContent />}
       <h1>Edit Chat</h1>
       {!!chat && <EditChatForm chat={chat} />}
     </div>
